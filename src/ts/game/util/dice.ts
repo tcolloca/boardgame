19972bd7a9ad18cc6b0639cc012623980b2f713e
Tom Colloca <tomascllc94@gmail.com>
2019-09-14 18:45:30 +0200

    Add dice class

---
+++ src/ts/game/util/dice.ts
@@ -0,0 +1,14 @@
+import { Die } from "./die";
+
+export class Dice {
+
+	private dice: Die[];
+
+    constructor(... sides: number[]) {
+      this.dice = sides.map(side => new Die(side));
+    }
+
+	roll(): number[] {
+      return this.dice.map(die => die.roll());
+	}
+}
