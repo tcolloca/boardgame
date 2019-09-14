2b921867962c4941ab96fc2229229fa56d809243
Tom Colloca <tomascllc94@gmail.com>
2019-09-14 18:53:09 +0200

    Remove src/js/*

--- src/js/game/game.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Game {
-    test(message) {
-        console.log(message);
-    }
-}
-exports.Game = Game;
-//# sourceMappingURL=game.js.map
