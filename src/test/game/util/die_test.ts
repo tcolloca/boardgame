86fe6fa70c1ee54a0786427b3fca49a1e1d8f725
Tomas Colloca Bargallo <tomascllc94@gmail.com>
2019-09-21 15:57:26 +0200

    Refactor and add proto support. (#12)

--- src/test/game/util/die_test.ts
+++ src/test/game/util/die_test.ts
@@ -10,22 +10,22 @@ describe('Die', () => {
   it('should default to a 6-sided die if `sides` is not set.', () => {
     const die = new Die();
     Array.from(Array(attempts).keys())
-      .map((unused) => die.roll())
-      .forEach((roll) => {
-        expect(roll).to.be.at.least(1);
-        expect(roll).to.be.at.most(6);
-      });
-  });
-
-  it('should roll between 1 and number of sides.', () => {
-    for (var sides = 1; sides <= 10; sides++) {
-      const die = new Die(sides);
-      Array.from(Array(attempts / 10).keys())
         .map((unused) => die.roll())
         .forEach((roll) => {
           expect(roll).to.be.at.least(1);
-          expect(roll).to.be.at.most(sides);
+          expect(roll).to.be.at.most(6);
         });
+  });
+
+  it('should roll between 1 and number of sides.', () => {
+    for (let sides = 1; sides <= 10; sides++) {
+      const die = new Die(sides);
+      Array.from(Array(attempts / 10).keys())
+          .map((unused) => die.roll())
+          .forEach((roll) => {
+            expect(roll).to.be.at.least(1);
+            expect(roll).to.be.at.most(sides);
+          });
     }
   });
 });
